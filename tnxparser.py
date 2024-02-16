db76a9bca0a23297309043487362d40f22f2100a
0xZaddyy <oyinoladapo5@gmail.com>
2024-02-16 01:31:16 +0100

    simplify codebase

--- tnxparser.py
+++ tnxparser.py
@@ -15,15 +15,10 @@ def parse_tx_input(s):
     prev_tx_hash = s[:32][::-1]  # Reverse for endianness
     output_index = struct.unpack('<I', s[32:36])[0]
     script_length, s = read_variant(s[36:])
-    script_sig = s[:script_length]
     sequence, s = struct.unpack('<I', s[script_length:script_length+4]), s[script_length+4:]
     return {
         'txid': prev_tx_hash.hex(),
         'vout': output_index,
-        'scriptSig': {
-            'hex': script_sig.hex()
-        },
-        'sequence': sequence
     }, s
 
 def parse_tx_output(s):
@@ -33,7 +28,7 @@ def parse_tx_output(s):
     s = s[script_length:]
     return {
         'value': value / 1e8,  # Convert satoshis to BTC
-        # 'n': index,
+     
         'scriptPubKey': {
             'hex': script_pub_key.hex()
         }
@@ -76,7 +71,7 @@ def parse_raw_transaction(hex_tx):
         'version': version,
         'locktime': locktime,
         'vin': inputs,
-        'vout': outputs
+       'vout': outputs 
     }
 
 hex_tx = "020000000001010ccc140e766b5dbc884ea2d780c5e91e4eb77597ae64288a42575228b79e234900000000000000000002bd37060000000000225120245091249f4f29d30820e5f36e1e5d477dc3386144220bd6f35839e94de4b9cae81c00000000000016001416d31d7632aa17b3b316b813c0a3177f5b6150200140838a1f0f1ee607b54abf0a3f55792f6f8d09c3eb7a9fa46cd4976f2137ca2e3f4a901e314e1b827c3332d7e1865ffe1d7ff5f5d7576a9000f354487a09de44cd00000000"
